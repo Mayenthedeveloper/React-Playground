cfc0962f468ced8acaa856eda5bbed7d08367535
Mayen Akpan <neroxma@yahoo.com>
2020-11-02 09:59:31 -0800

    updated 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-=======
 // import { render, screen } from '@testing-library/react';
 // import App from './App';
 
@@ -35,6 +26,5 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 
   // clean up code
->>>>>>> 1ef8d2c90d365cd1ca2b1c2967ca362a1ae697f8
   ReactDOM.unmountComponentAtNode(div);
 });
