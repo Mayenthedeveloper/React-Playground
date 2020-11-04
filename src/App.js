bc8dcc471e26debe9204dbfb898c3c178ab113e7
Mayen Akpan <neroxma@yahoo.com>
2020-11-03 21:38:09 -0800

    Updated state-drill

--- src/App.js
+++ src/App.js
@@ -35,22 +35,48 @@
 // export default App
 
 
-import React, { Component } from 'react';
-import './App.css';
-import Messages from './Messages';
+// import React, { Component } from 'react';
+// import './App.css';
+// import Messages from './Messages';
+// import TheDate from './state/TheDate';
+// import Counter from './state/Counter';
 
-class App extends Component {
+// class App extends Component {
+//   render(){
+//     return(
+//       <div className='App'>
+//         <h1> My Name is Mayen Akpan!</h1>
+//         <Messages name ="Messages" unread={0}/>
+//         <Messages name ="Notifications" unread ={10}/>
+    
+//       </div>
+      
+//     )
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from "./state-drills/Bomb";
+import RouletteGun from './state-drills/RouletteGun';
+import './App.css';
+
+class App extends Component{
   render(){
     return(
-      <div className='App'>
-        <h1> My Name is Mayen Akpan!</h1>
-        <Messages name ="Messages" unread={0}/>
-        <Messages name ="Notifications" unread ={10}/>
-    
+      <div className ='App'>
+        {/* <h2>Hello World</h2> */}
+        <HelloWorld />
+        {/* <h2>Bomb</h2> */}
+        <Bomb />
+        {/* <h2>Roulette Gun</h2> */}
+        <RouletteGun bullettInchamber={6} />
       </div>
-      
-    )
+    );
   }
 }
 
 export default App;
+
