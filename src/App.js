6afac9ca70d9871a0bb810c97431ee500a52c668
Mayen Akpan <neroxma@yahoo.com>
2020-11-04 22:25:48 -0800

    Added Accordion

--- src/App.js
+++ src/App.js
@@ -57,11 +57,77 @@
 
 // export default App;
 
+// import React, { Component } from 'react';
+// import HelloWorld from './state-drills/HelloWorld';
+// import Bomb from "./state-drills/Bomb";
+// import RouletteGun from './state-drills/RouletteGun';
+// import './App.css';
+// import Tabs from './state/Tabs';
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <div className ='App'>
+//         {/* <h2>Hello World</h2> */}
+//         <HelloWorld />
+//         {/* <h2>Bomb</h2> */}
+//         <Bomb />
+//         {/* <h2>Roulette Gun</h2> */}
+//         <RouletteGun bullettInchamber={6} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// import React from "react";
+// import Tabs from './state/Tabs';
+// import accordian from './accordian/Accordian';
+
+// const tabsProp = [
+//   { name: 'First tab',
+//     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+//   { name: 'Second tab',
+//     content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+//   { name: 'Third tab',
+//     content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+// ];
+
+// class App extends React.Component{
+// render(){
+//   return <div>
+//     <Tabs tabs={tabsProp} />
+//   </div>
+//   }
+// }
+// export default App
+
+
+
 import React, { Component } from 'react';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from "./state-drills/Bomb";
 import RouletteGun from './state-drills/RouletteGun';
 import './App.css';
+import Tabs from './state/Tabs';
+import Accordion from './accordion/Accordion';
+
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 class App extends Component{
   render(){
@@ -73,10 +139,11 @@ class App extends Component{
         <Bomb />
         {/* <h2>Roulette Gun</h2> */}
         <RouletteGun bullettInchamber={6} />
+        {/* <h2>Accordian</h2> */}
+        <Accordion  sections={sections} />
       </div>
     );
   }
 }
 
 export default App;
-
